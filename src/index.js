5c00e87d683d57fb00b341e66c80b3d349a0d688
fricojgm <franciscojosegenao@gmail.com>
2025-07-09 08:55:13 -0400

    df

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-const express = require('express');
+onst express = require('express');
 const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/reporte-mercado/:ticker', async (req, res) => {
     const { ticker } = req.params;
-    const puenteURL = `https://jarvis-libre.onrender.com/reporte-mercado/${ticker}`;
+    const hoy = new Date().toISOString().split('T')[0];
+    const apiKey = 'PxOMBWjCFxSbfan_jH9LAKp4oA4Fyl3V';
+    const puenteURL = `https://api.polygon.io/v1/open-close/${ticker}/${hoy}?apiKey=${apiKey}`;
 
     try {
         const response = await axios.get(puenteURL);
-
         if (response.status === 200 && response.data) {
             return res.json(response.data);
         } else {
